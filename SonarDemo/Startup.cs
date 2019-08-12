b44c42bb357a3744e708286f33e0d2a706ab95fc
jegatheeswaran <jegatheeswaran@NTPL-LT067>
2019-08-12 15:47:31 +0530

    Add project files.

---
+++ SonarDemo/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SonarDemo.Startup))]
+namespace SonarDemo
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
